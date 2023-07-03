fe7b2ba58d13d78bbd7d16067d245d589608b526
Julia Mersing Ortiz <mariajulia.mersing@clearpay.com>
2023-07-03 08:58:07 +0200

    feat(setup): adding basic setup

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface HeaderProps {
+	heading: string;
+	href: string;
+	linkParagraph: string;
+}
+
+export const Header: React.FunctionComponent<HeaderProps> = ({
+	heading,
+	href,
+	linkParagraph,
+}) => (
+	<>
+		<h2 className="heading">{heading}</h2>
+		<a href={href} className="link">
+			{linkParagraph}
+		</a>
+	</>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Header } from '../components/Header/Header';
+
+export const Home: React.FunctionComponent = () => (
+	<Header heading="Contacts" href="/" linkParagraph="Go back to address book" />
+);
